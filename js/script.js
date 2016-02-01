d1b5b18b0e995303c6d36360ca31b5ef648c1205
commander-clifford <droffilcnoslen@gmail.com>
2016-01-31 20:38:02 -0800

    its a start

--- js/script.js
+++ js/script.js
@@ -1,2 +1,41 @@
+function fitui(){
+	var winWid = $(window).width();
+	var winHei = $(window).height();
+	var jumbotron = $("#jumbotron .container");
 
-alert("Hello World!");
+
+	jumbotron.height(winHei-150);
+
+}
+$(document).ready(function(){
+	fitui();
+	$('body').scrollspy({ 
+		target: '#navbar',
+		offset: 100
+	});
+
+
+
+
+});
+$(window).resize(function(){
+	fitui();
+});
+
+
+
+
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top - 100 // navbar height
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
